e190472179bf8b722f5593a9d6c38cd4785e339d
AmoebaProtozoa <8039876+AmoebaProtozoa@users.noreply.github.com>
2021-12-30 23:03:09 +0800

    simplify run.sh for the demo

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ BASE_PATH=$(cd "$(dirname "$0")" || exit 1; pwd)
 
 # check if run-time variable exist
 if [ ! -d "${BASE_PATH}"/config ] || [ ! -f "${BASE_PATH}"/config/load_vars.sh ]; then
-    echo -e "error!\t fail to find runtime variables, please check if the file 'config/load_variables.sh' exist in the current path."
-    echo "failed to run!"
+    echo "[run.sh] error! fail to load runtime variables"
+    echo "[run.sh] failed to run!"
     exit 1
 fi
 
@@ -21,42 +21,19 @@ source "${BASE_PATH}"/config/load_variables.sh
 ##==============================================================================================
 
 if [[ $(whoami) != "${OS_USER}" ]]; then
-  echo -e "error!\t current user is not ${OS_USER}!"
-  echo -e "\t please use ${OS_USER} to run this script."
-  echo "failed to run!"
+  echo "[run.sh] error! current user is not ${OS_USER}!"
+  echo "[run.sh] failed to run!"
   exit 1;
 fi
-
-##==============================================================================================
-## set up environment
-##==============================================================================================
-
-# add /usr/bin to PATH in case we can't find mysql binary when executing from crontab
-export PATH=/usr/bin:$PATH
-
-##==============================================================================================
-## cleanup old files
-##==============================================================================================
-sh "${BASE_PATH}"/cleanup.sh "${BASE_PATH}" "${REPORT_CLEANING_INTERVAL}"|| exit 1
 
 ##==============================================================================================
 ## executing all check scripts formats print
 ## this will also generate report in /report and error report in err_report/
 ##==============================================================================================
-if python "${BASE_PATH}"/geenrate_report.py "$BASE_PATH" "$MYSQL_LOGIN_PATH" "$PROMETHEUS_ADDRESSES"
+if python "${BASE_PATH}"/generate_report.py "$BASE_PATH" "$MYSQL_LOGIN_PATH" "$PROMETHEUS_ADDRESSES"
 then
-  echo "success!"
+  echo "[run.sh] success!"
 else
-  echo -e "error!\t fail to execute check scripts."
+  echo "[run.sh] error! failed to run generate_report.py"
   exit 1
 fi
-
-##==============================================================================================
-## update baselines with the latest report
-##==============================================================================================
-sh "${BASE_PATH}"/update_baselines.sh "${BASE_PATH}" || exit 1
-
-##==============================================================================================
-## upload to ftp server
-##==============================================================================================
-sh "${BASE_PATH}"/upload_to_ftp.sh "${BASE_PATH}" "${FTP_SERVER}"|| exit 1
