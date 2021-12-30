cb47e26034aa6ab0f7cec3cc899ea9e79511cac7
hey-hoho <591310381@qq.com>
2021-12-30 15:35:00 +0800

    update execute check

--- insight/controllers/handler/report_handler.go
+++ insight/controllers/handler/report_handler.go
@@ -1,10 +1,11 @@
 package handler
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 var upGrader = websocket.Upgrader{
@@ -33,24 +34,32 @@ func (r *ReportHandler) GetMeta(c *gin.Context) {
 
 func (r *ReportHandler) ExecuteCheck(c *gin.Context) {
 	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
-	if err !=nil {
+	if err != nil {
 		return
 	}
 
 	defer ws.Close()
-
+	i := 0
 	for {
 
-		err = ws.WriteJSON(nil)
-
-		if err != nil {
+		err = ws.WriteJSON(map[string]interface{}{
+			"finished":        true,
+			"check_class":     "集群",
+			"check_name":      "存活的TiDB数量",
+			"check_item":      "TiDB节点数",
+			"check_result":    "正常",
+			"check_value":     5,
+			"check_threshold": "等于5",
+			"check_time":      20211221063030,
+		})
+		i++
+		if err != nil || i >= 10 {
 			break
 		}
 
-		time.Sleep(time.Second)
+		time.Sleep(time.Second * 3)
 
 	}
-
 
 	return
 }
