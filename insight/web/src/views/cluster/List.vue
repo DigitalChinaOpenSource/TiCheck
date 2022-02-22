fa22aaf6038ad83012baf6cf934af8274ae6f501
hey-hoho <591310381@qq.com>
2022-02-22 16:38:21 +0800

    update  frontend framework

---
+++ insight/web/src/views/cluster/List.vue
@@ -0,0 +1,245 @@
+<template>
+  <page-header-wrapper>
+    <div class="page-header-wrapper">
+    <div>
+      <a-row :gutter="24">
+        <a-col :xl="16" :lg="24" :md="24" :sm="24" :xs="24">
+          <a-card
+            class="project-list"
+            :loading="loading"
+            style="margin-bottom: 24px;"
+            :bordered="false"
+            title="进行中的项目"
+            :body-style="{ padding: 0 }"
+          >
+            <a slot="extra">全部项目</a>
+            <div>
+              <a-card-grid class="project-card-grid" :key="i" v-for="(item, i) in projects">
+                <a-card :bordered="false" :body-style="{ padding: 0 }">
+                  <a-card-meta>
+                    <div slot="title" class="card-title">
+                      <a-avatar size="small" :src="item.cover" />
+                      <a>{{ item.title }}</a>
+                    </div>
+                    <div slot="description" class="card-description">
+                      {{ item.description }}
+                    </div>
+                  </a-card-meta>
+                  <div class="project-item">
+                    <a href="/#/">科学搬砖组</a>
+                    <span class="datetime">9小时前</span>
+                  </div>
+                </a-card>
+              </a-card-grid>
+            </div>
+          </a-card>
+
+          <a-card :loading="loading" title="动态" :bordered="false">
+            <a-list>
+              <a-list-item :key="index" v-for="(item, index) in activities">
+                <a-list-item-meta>
+                  <a-avatar slot="avatar" size="small" :src="item.user.avatar" />
+                  <div slot="title">
+                    <span>{{ item.user.nickname }}</span
+                    >&nbsp; 在&nbsp;<a href="#">{{ item.project.name }}</a
+                    >&nbsp; <span>{{ item.project.action }}</span
+                    >&nbsp;
+                    <a href="#">{{ item.project.event }}</a>
+                  </div>
+                  <div slot="description">{{ item.time }}</div>
+                </a-list-item-meta>
+              </a-list-item>
+            </a-list>
+          </a-card>
+        </a-col>
+        <a-col
+          style="padding: 0 12px"
+          :xl="8"
+          :lg="24"
+          :md="24"
+          :sm="24"
+          :xs="24">
+          <a-card
+            title="快速开始 / 便捷导航"
+            style="margin-bottom: 24px"
+            :bordered="false"
+            :body-style="{ padding: 0 }"
+          >
+            <div class="item-group">
+              <a>操作一</a>
+              <a>操作二</a>
+              <a>操作三</a>
+              <a>操作四</a>
+              <a>操作五</a>
+              <a>操作六</a>
+              <a-button size="small" type="primary" ghost icon="plus">添加</a-button>
+            </div>
+          </a-card>
+          <a-card
+            title="XX 指数"
+            style="margin-bottom: 24px"
+            :loading="radarLoading"
+            :bordered="false"
+            :body-style="{ padding: 0 }"
+          >
+            <div style="min-height: 400px;">
+              <!-- :scale="scale" :axis1Opts="axis1Opts" :axis2Opts="axis2Opts"  -->
+              <radar :data="radarData" />
+            </div>
+          </a-card>
+          <a-card :loading="loading" title="团队" :bordered="false">
+            <div class="members">
+              <a-row>
+                <a-col :span="12" v-for="(item, index) in teams" :key="index">
+                  <a>
+                    <a-avatar size="small" :src="item.avatar" />
+                    <span class="member">{{ item.name }}</span>
+                  </a>
+                </a-col>
+              </a-row>
+            </div>
+          </a-card>
+        </a-col>
+      </a-row>
+    </div></div>
+  </page-header-wrapper>
+</template>
+
+<script>
+import { timeFix } from '@/utils/util'
+import { mapState } from 'vuex'
+import { PageHeaderWrapper } from '@ant-design-vue/pro-layout'
+import { Radar } from '@/components'
+
+import { getRoleList, getServiceList } from '@/api/manage'
+
+const DataSet = require('@antv/data-set')
+
+export default {
+  name: 'Workplace',
+  components: {
+    PageHeaderWrapper,
+    Radar
+  },
+  data () {
+    return {
+      timeFix: timeFix(),
+      avatar: '',
+      user: {},
+
+      projects: [],
+      loading: true,
+      radarLoading: true,
+      activities: [],
+      teams: [],
+
+      // data
+      axis1Opts: {
+        dataKey: 'item',
+        line: null,
+        tickLine: null,
+        grid: {
+          lineStyle: {
+            lineDash: null
+          },
+          hideFirstLine: false
+        }
+      },
+      axis2Opts: {
+        dataKey: 'score',
+        line: null,
+        tickLine: null,
+        grid: {
+          type: 'polygon',
+          lineStyle: {
+            lineDash: null
+          }
+        }
+      },
+      scale: [
+        {
+          dataKey: 'score',
+          min: 0,
+          max: 80
+        }
+      ],
+      axisData: [
+        { item: '引用', a: 70, b: 30, c: 40 },
+        { item: '口碑', a: 60, b: 70, c: 40 },
+        { item: '产量', a: 50, b: 60, c: 40 },
+        { item: '贡献', a: 40, b: 50, c: 40 },
+        { item: '热度', a: 60, b: 70, c: 40 },
+        { item: '引用', a: 70, b: 50, c: 40 }
+      ],
+      radarData: []
+    }
+  },
+  computed: {
+    ...mapState({
+      nickname: state => state.user.nickname,
+      welcome: state => state.user.welcome
+    }),
+    currentUser () {
+      return {
+        name: 'Serati Ma',
+        avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png'
+      }
+    },
+    userInfo () {
+      return this.$store.getters.userInfo
+    }
+  },
+  created () {
+    this.user = this.userInfo
+    this.avatar = this.userInfo.avatar
+
+    getRoleList().then(res => {
+      // console.log('workplace -> call getRoleList()', res)
+    })
+
+    getServiceList().then(res => {
+      // console.log('workplace -> call getServiceList()', res)
+    })
+  },
+  mounted () {
+    this.getProjects()
+    this.getActivity()
+    this.getTeams()
+    this.initRadar()
+  },
+  methods: {
+    getProjects () {
+      this.$http.get('/list/search/projects').then(res => {
+        this.projects = res.result && res.result.data
+        this.loading = false
+      })
+    },
+    getActivity () {
+      this.$http.get('/workplace/activity').then(res => {
+        this.activities = res.result
+      })
+    },
+    getTeams () {
+      this.$http.get('/workplace/teams').then(res => {
+        this.teams = res.result
+      })
+    },
+    initRadar () {
+      this.radarLoading = true
+
+      this.$http.get('/workplace/radar').then(res => {
+        const dv = new DataSet.View().source(res.result)
+        dv.transform({
+          type: 'fold',
+          fields: ['个人', '团队', '部门'],
+          key: 'user',
+          value: 'score'
+        })
+
+        this.radarData = dv.rows
+        this.radarLoading = false
+      })
+    }
+  }
+}
+</script>
