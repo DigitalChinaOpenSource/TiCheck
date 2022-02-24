78bab8471be6fa2157fb7f80f5e6b6af70222b8a
pupillord <945317899@qq.com>
2022-02-24 18:35:51 +0800

    added init.sql

--- insight/web/vue.config.js
+++ insight/web/vue.config.js
@@ -104,8 +104,8 @@ const vueConfig = {
     port: 8000,
     // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
     proxy: {
-      '/api': {
-        target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
+      '/': {
+        target: 'http://localhost:8081/',
         ws: false,
         changeOrigin: true
       }
