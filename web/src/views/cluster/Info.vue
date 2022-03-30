094d016fd28ad8ea99567784fd39484dfdc5c639
hey-hoho <591310381@qq.com>
2022-03-30 15:44:41 +0800

    update cluster info page

--- web/src/views/cluster/Info.vue
+++ web/src/views/cluster/Info.vue
@@ -10,11 +10,11 @@
         }}</a-button>
       </template>
     </a-page-header>
-    <a-row :gutter="24" :style="{ marginTop: '5px' }">
+    <a-row :gutter="24" :style="{ margin: '5px 0px 0px 0px',background:'#ECECEC' }">
       <a-col :sm="24" :md="24" :xl="24" :style="{ marginBottom: '4px' }">
         <div>
-          <a-card :style="{ marginTop: '5px' }">
-            <a-descriptions title="" size="middle">
+          <a-card :style="{ marginTop: '12px' }">
+            <a-descriptions title="" size="default">
               <a-descriptions-item :label="$t('cluster.info.name')">{{
                 clusterInfo.name
               }}</a-descriptions-item>
@@ -27,7 +27,7 @@
               <a-descriptions-item :label="$t('cluster.info.owner')" span="3">{{
                 clusterInfo.owner
               }}</a-descriptions-item>
-              <a-descriptions-item :label="$t('cluster.info.description')">{{
+              <a-descriptions-item :label="$t('cluster.info.description')" span="3">{{
                 clusterInfo.description
               }}</a-descriptions-item>
             </a-descriptions>
@@ -35,68 +35,115 @@
         </div>
       </a-col>
     </a-row>
-    <a-page-header
+    <!-- <a-page-header
       style="
         margin-top: 24px;
         border: 1px solid rgb(235, 237, 240);
         margin-bottom: 20px;
       "
       :title="$t('cluster.info.status')"
-    />
-    <a-row :gutter="24" :style="{ marginTop: '5px' }">
+    /> -->
+    <a-row :gutter="24" :style="{ margin: '0px',background:'#ECECEC', padding:'15px 0px' }">
       <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '4px' }">
-        <a-card
+        <!-- <a-card
           :title="$t('cluster.info.status.count')"
           style="text-align: center"
         >
           <span>
             {{ clusterInfo.check_count }}
           </span>
-        </a-card>
+        </a-card> -->
+        <chart-card :loading="false" :title="$t('cluster.info.status.count')" :total="clusterInfo.check_count | NumberFormat">
+          <a-tooltip :title="$t('dashboard.analysis.introduce')" slot="action">
+            <a-icon type="info-circle-o" />
+          </a-tooltip>
+          <div>
+            <mini-area />
+          </div>
+          <template slot="footer">日巡检次数<span> {{ '1234' | NumberFormat }}</span></template>
+        </chart-card>
       </a-col>
       <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '4px' }">
-        <a-card
+        <!-- <a-card
           :title="$t('cluster.info.status.total')"
           style="text-align: center"
         >
           <span>
             {{ clusterInfo.check_total }}
           </span>
-        </a-card>
+        </a-card> -->
+        <chart-card :loading="false" :title="$t('cluster.info.status.total')" :total="clusterInfo.check_total | NumberFormat">
+          <a-tooltip :title="$t('dashboard.analysis.introduce')" slot="action">
+            <a-icon type="info-circle-o" />
+          </a-tooltip>
+          <div>
+            <mini-area />
+          </div>
+          <template slot="footer">日检查项<span> {{ '1234' | NumberFormat }}</span></template>
+        </chart-card>
       </a-col>
       <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '4px' }">
-        <a-card
+        <!-- <a-card
           :title="$t('cluster.info.status.last')"
           style="text-align: center"
         >
           <span>{{ clusterInfo.last_check_time | moment }}</span>
-        </a-card>
+        </a-card> -->
+        <chart-card :loading="false" :title="$t('cluster.info.status.last')" :total="clusterInfo.last_check_time | moment">
+          <a-tooltip :title="$t('dashboard.analysis.introduce')" slot="action">
+            <a-icon type="info-circle-o" />
+          </a-tooltip>
+          <template slot="footer">
+            <trend flag="down" style="margin-right: 16px;">
+              <span slot="term">正常数：</span>
+              12
+            </trend>
+            <trend flag="up">
+              <span slot="term">告警数：</span>
+              80
+            </trend>
+          </template>
+        </chart-card>
       </a-col>
       <a-col :sm="24" :md="12" :xl="6" :style="{ marginBottom: '4px' }">
-        <a-card
+        <!-- <a-card
           :title="$t('cluster.info.status.healthy')"
           style="text-align: center"
         >
           <span>
             {{ clusterInfo.cluster_health }}
           </span>
-        </a-card>
+        </a-card> -->
+        <chart-card :loading="false" :title="$t('cluster.info.status.healthy')" :total="clusterInfo.cluster_health">
+          <a-tooltip :title="$t('dashboard.analysis.introduce')" slot="action">
+            <a-icon type="info-circle-o" />
+          </a-tooltip>
+          <div>
+            <mini-progress color="rgb(19, 194, 194)" :target="80" :percentage="clusterInfo.cluster_health" height="8px" />
+          </div>
+          <template slot="footer">
+            <trend flag="down" style="margin-right: 16px;">
+              <span slot="term">更新时间：</span>
+              12：30
+            </trend>
+          </template>
+        </chart-card>
       </a-col>
     </a-row>
-    <a-page-header
+    <!-- <a-page-header
       style="
         margin-top: 24px;
         border: 1px solid rgb(235, 237, 240);
         margin-bottom: 20px;
       "
       :title="$t('cluster.info.recent')"
-    />
-    <a-card :bordered="false" :body-style="{ padding: '0' }">
+    /> -->
+    <a-card :bordered="false" style="marginTop:20px" :body-style="{ padding: '0' }" :title="$t('cluster.info.recent')">
       <div class="salesCard">
         <a-row>
           <a-col :xl="16" :lg="12" :md="12" :sm="24" :xs="24">
             <bar
-              :style="{ marginTop: '24px' }"
+              :style="{ padding: '0' }"
               :data="clusterInfo.recent_warning_items"
             />
           </a-col>
@@ -104,25 +151,25 @@
             <a-tabs
               default-active-key="1"
               size="large"
-              :tab-bar-style="{ marginBottom: '24px', paddingLeft: '16px' }"
+              :tab-bar-style="{ margin: '0 0 0 20px' }"
               style="color: #40a9ff"
             >
               <a-tab-pane loading="true" tab="weekly" key="1">
                 <rank-list
                   :list="clusterInfo.weekly_history_warnings"
-                  :style="{ marginTop: '24px' }"
+                  :style="{ padding: '0 22px' }"
                 />
               </a-tab-pane>
               <a-tab-pane tab="monthly" key="2">
                 <rank-list
                   :list="clusterInfo.monthly_history_warnings"
-                  :style="{ marginTop: '24px' }"
+                  :style="{ padding: '0 22px' }"
                 />
               </a-tab-pane>
               <a-tab-pane tab="yearly" key="3">
                 <rank-list
                   :list="clusterInfo.yearly_history_warnings"
-                  :style="{ marginTop: '24px' }"
+                  :style="{ padding: '0 22px' }"
                 />
               </a-tab-pane>
             </a-tabs>
@@ -135,9 +182,8 @@
 
 <script>
 import { getClusterInfo } from "@/api/cluster";
-import { ChartCard, RankList, Bar } from "@/components";
+import { ChartCard, RankList, Bar, MiniProgress, MiniArea } from "@/components";
 import moment from "moment";
-// import moment from "moment";
 const clusterInfo = {};
 export default {
   name: "ClusterInfo",
@@ -146,6 +192,8 @@ export default {
     ChartCard,
     RankList,
     Bar,
+    MiniProgress,
+    MiniArea,
   },
   data() {
     return {
